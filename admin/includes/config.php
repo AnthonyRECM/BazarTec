742f268c83ea0734d9587d6991a99cb1c57a93d6
AnthonyRECM <55anthonyhernandez55@gmail.com>
2024-04-14 19:35:45 -0600

    b

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+  define( 'DB_PASS', '020505' );             // Set database password
+  define( 'DB_NAME', 'BazarTec' );        // Set database name
 
 ?>
