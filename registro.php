f7d2cad2c52584dc0c3d9f7280217234772b2bd9
Lesliesaalgado <94340659+Lesliesaalgado@users.noreply.github.com>
2024-04-30 23:14:38 -0600

    cambios x20

--- registro.php
+++ registro.php
@@ -15,10 +15,8 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="fontawesome/css/all.css">
     <link rel="shortcut icon" href="/images/logo.png" width="30" height="30" type="img">
-    <link rel="stylesheet" type="text/css" href="css/registro.css">
+    <link rel="stylesheet" type="text/css" href="./css/registro.css">
 </head>
-
-
 
 <body>
     <!--Header-->
@@ -28,7 +26,7 @@
                 <nav class="navbar navbar-expand-lg " style="background-color:#325288;">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="index.html" style="color: white;">
-                            <img src="/images/logo.png" alt="" width="50" height="50" class=" d-inline-block ">
+                            <img src="./images/logo.png" alt="" width="50" height="50" class=" d-inline-block ">
                             BazarTec
                         </a>
                         <div class="collapse navbar-collapse">
@@ -53,7 +51,7 @@
         <div class="row">
             <div class="col-7">
                 <div class="container formatoRegistro ">
-                    <form class="form needs-validation" novalidate action="" method="post">
+                    <form class="form needs-validation" novalidate action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="row">
                             <div class="col-12" style="padding: 2rem;">
                                 <h2> CREA TU CUENTA</h2>
@@ -64,8 +62,8 @@
                                 </div>
                             </div>
                             <div class="col-6">
-                                <input type="text" name="apellido" placeholder="Apellido" id="apellidoCliente" required />
-                                <div class="invalid-feedback">Por favor, ingresa tu apellido.
+                                <input type="text" name="apellido" placeholder="Apellidos" id="apellidoCliente" required />
+                                <div class="invalid-feedback">Por favor, ingresa tus apellidos.
                                 </div>
                             </div>
                             <div class="col-6">
@@ -75,14 +73,14 @@
                                 </div>
                             </div>
                             <div class="col-6">
-                                <input type="password" name="clave" placeholder="Contraseña" id="contraseñaPaciente"
+                                <input type="password" name="clave" placeholder="Contraseña" id="contraseñaCliente"
                                     required />
                                 <div class="invalid-feedback">Por favor, ingresa tu contraseña.
                                 </div>
                             </div>
     
                             <div class="d-grid gap-2 col-6 mx-auto" style="padding: 1rem;">
-                                <button type="submit" href="./registro.html" value="Iniciar Sesión" type="button" name="btningresarRegistro"><i
+                                <button type="submit" href="./login.html" value="Iniciar Sesión" type="button" name="btningresarRegistro"><i
                                         class="fa-solid fa-pen-to-square" style="color: #ffffff;"></i> Crear
                                     cuenta</button>
                             </div>
@@ -107,3 +105,32 @@
 </body>
 
 </html>
+
+<?php
+include './php/conexionDB.php';
+// Verificar la conexión
+if (mysqli_connect_errno()) {
+    echo 'No se pudo conectar a la base de datos : ' . mysqli_connect_error();
+} else {
+    // Verificar si se han enviado datos del formulario
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Procesar y limpiar los datos del formulario
+    
+        $nombre = mysqli_real_escape_string($con, $_POST['nombree']);
+        $apellido = mysqli_real_escape_string($con, $_POST['apellido']);
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $clave = mysqli_real_escape_string($con, $_POST['clave']);
+
+        // Consulta SQL para insertar un nuevo cliente
+        $sql = "INSERT INTO clientes (nombre, apellido, email, clave) VALUES ('$nombre', '$apellido', '$email', '$clave')";
+
+        // Ejecutar la consulta y verificar si fue exitosa
+        if (mysqli_query($con, $sql)) {
+            echo 'Cliente insertado correctamente.';
+        } else {
+            echo 'Error al insertar el cliente: ' . mysqli_error($con);
+        }
+    }
+    mysqli_close($con); // Cerrar la conexión
+}
+?>
