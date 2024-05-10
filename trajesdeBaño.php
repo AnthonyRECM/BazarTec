79e5544d66e384315b91931d6406a08b0fc2188a
Narunakyum <yiskah_naru17@hotmail.com>
2024-05-10 15:27:24 -0600

    modif

--- trajesdeBaño.php
+++ trajesdeBaño.php
@@ -6,7 +6,7 @@ include './php/conexionDB.php';
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./fontawesome/css/all.css">
