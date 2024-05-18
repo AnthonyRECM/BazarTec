aad7566850b70fa7b217ace23b18b25c31601201
Narunakyum <yiskah_naru17@hotmail.com>
2024-05-18 16:22:30 -0600

    h

--- trajesdeBaño.php
+++ trajesdeBaño.php
@@ -91,24 +91,66 @@ include './php/conexionDB.php';
                 </div>
            </div>
 
-            <div class="row text-center desplazarMov">
+           <!-- <div class="row text-center desplazarMov">
                 <div class="col-12 mt-3">
                     <p class="letras1">Ver mas</p>
                 </div>
                 <div class="col mb-2">
                     <i class="fa-solid fa-chevron-down fa-lg"></i>
                 </div>
-            </div>
+            </div>-->
         </div>
     </div>
+
+    <br>
+    
+    <div class="container fondo3 my-5 py-3">
+        <?php
+            $query = mysqli_query($con, 'SELECT p.id,p.name,p.quantity,p.sale_price,p.media_id,c.name AS categorie,m.file_name AS image
+            FROM products p LEFT JOIN categories c ON c.id = p.categorie_id LEFT JOIN media m ON m.id = p.media_id
+             WHERE categorie_id=1 ORDER BY date DESC LIMIT 1');
+        ?>
+        <div class="row px-4"> 
+            <div class="col-12">
+                <p class="subtitulo3"> Lo mas nuevo en tendecias </p>
+            </div>
+        </div>
+        <div class="row ps-4">
+            <div class="col-6 text-center">
+            <?php
+                while ($consulta = mysqli_fetch_array($query)) {
+                    echo '
+                        <p class="p1">! Compralo ya ! </p>
+                        <p class="p2">'.$consulta['name'].'</p>
+                        <p class="p1">A un increible precio de: </p>
+                        <p class="p2">¡¡¡ '.$consulta['sale_price'].' !!!</p>
+                    </div>
+                    <div class="col-6 text-center">';
+                        if($consulta['media_id'] === '0'):
+                            echo '<img style="width: auto; height: 100%" class="img-avatar img-circle" src="./admin/uploads/products/no_image.jpg" alt="">';
+                        else:
+                            echo "<img class='img-avatar img-circle imgNuevo' src='./admin/uploads/products/".$consulta['image'].    "' alt=''>";
+                        endif;
+                    echo '</div>
+                    ';
+                }
+            ?>
+           
+        </div>
+    </div>
+
+    <br>
+    
+
+
     <div class="container-fluid fondo2"> 
-        <div id="desplazarDestino" class="row my-5">
+       <!-- <div id="desplazarDestino" class="row my-5">
             <div class="col text-center">
                 <h2 class="subtitulo">¡Trajes de baño para toda la familia!</h2>
             </div>
-        </div>
+        </div>-->
 
-    
+        
         <div class="row justify-content-evenly my-5">
 
          <?php
@@ -152,6 +194,8 @@ include './php/conexionDB.php';
             ?>
       </div>
    </div>
+   
+
     <!--footer-->
     <footer style="background-color: #24A19C; color: #fff; padding: 20px; width: 100%; text-align: center;">
         <p style="margin: 0;">&copy; 2024 BazarTec. Todos los derechos reservados. | <a href="#" style="color: #fff; text-decoration: none;">Política de privacidad</a> | <a href="#" style="color: #fff; text-decoration: none;">Términos y condiciones</a></p>
